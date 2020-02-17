43b350fc395df053794e636b1472d3785f8c523d
Kumovic Nenad <nenad.kumovic@it4communication.ch>
2020-02-17 20:12:45 +0100

    Small clean up of code

--- app/core/models.py
+++ app/core/models.py
@@ -12,7 +12,7 @@ class UserManager(BaseUserManager):
         """Creates and saves a new user"""
 
         if not email:
-          raise ValueError('User must have email address!')
+            raise ValueError('User must have email address!')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -37,7 +37,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
+    # is_superuser is included in PermissionsMixin
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
